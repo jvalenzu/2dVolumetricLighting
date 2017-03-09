bf66f2fc677426e512288cbac192cfebfeed4f09
Joe Valenzuela <jvalenzu@infinite-monkeys.org>
2017-03-09 08:30:07 -0800

    Small cleanup
    Update .gitignore, remove trailing whitespace in build rules

--- Build/rules.mk
+++ Build/rules.mk
@@ -59,7 +59,7 @@ $(call shaderOutName,$1) : obj/dummy obj/Shader/dummy $1 $(call shaderDepName,$1
 #	cpp -MMD -IRender/Shaders -MF$(call shaderDepName,$1) $1 $(call shaderOutName,$1)
 	-Build/Binary/mcpp -P -@old -MMD -IRender/Shaders -MF $(call shaderDepName,$1) $1 $(call shaderOutName,$1)
 
-$(call shaderDepName,$1) : $1 
+$(call shaderDepName,$1) : $1
 
 # depname
 -include $(call shaderDepName,$1)
